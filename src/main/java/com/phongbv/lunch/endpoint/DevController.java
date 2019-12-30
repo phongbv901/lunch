20ecec66a9fbadb7ed8c82aa1d86b53c91219633
phongbv901 <phongbv901@gmail.com>
2019-12-30 10:46:11 +0700

    Add log config add new endpoint to inspect request from slack

--- src/main/java/com/phongbv/lunch/endpoint/DevController.java
+++ src/main/java/com/phongbv/lunch/endpoint/DevController.java
@@ -1,5 +1,7 @@
 package com.phongbv.lunch.endpoint;
 
+import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -12,10 +14,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@Slf4j
 public class DevController {
 
+    private Gson gson = new Gson();
+
     @RequestMapping(value = "/requestInspect", method = RequestMethod.POST)
-    public Map<String, Object> getRequest(HttpEntity<String> httpEntity,
+    public Map<String, Object> inspectCommand(HttpEntity<String> httpEntity,
                           @RequestHeader MultiValueMap<String, String> headers,
                           HttpServletRequest request){
         Map<String, Object> output = new HashMap<>();
@@ -24,6 +29,21 @@ public class DevController {
         output.put("cookies", request.getCookies());
         output.put("clientInfo", request.getRemoteAddr());
         output.put("params", request.getParameterMap());
+        log.info("Request inspect details: {}", output);
+        return output;
+    }
+
+    @RequestMapping(value = "/interactive", method = RequestMethod.POST)
+    public Map<String, Object> inspectInteractive(HttpEntity<String> httpEntity,
+                                          @RequestHeader MultiValueMap<String, String> headers,
+                                          HttpServletRequest request){
+        Map<String, Object> output = new HashMap<>();
+        output.put("body", httpEntity != null ? httpEntity.getBody() : "");
+        output.put("headers",headers);
+        output.put("cookies", request.getCookies());
+        output.put("clientInfo", request.getRemoteAddr());
+        output.put("params", request.getParameterMap());
+        log.info("Request inspect details: {}", gson.toJson(output));
         return output;
     }
 }
